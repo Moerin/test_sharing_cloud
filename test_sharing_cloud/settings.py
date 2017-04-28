8cd50be236009719013e8b7a525eef6d0e8f8c51
SebCharret <sebastien.charret@lengow.com>
2017-04-28 09:18:55 +0200

    Fix missing STATIC_ROOT

--- test_sharing_cloud/settings.py
+++ test_sharing_cloud/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
