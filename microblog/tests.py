c5817f50435e247ddc3c4c186b0b862dae086292
SebCharret <sebastien.charret@lengow.com>
2017-04-28 09:08:35 +0200

    Fix unittest

--- microblog/tests.py
+++ microblog/tests.py
@@ -7,7 +7,7 @@ class PostTestCase(TestCase):
     def setUp(self):
         pass
 
-    def test_index(self):
+    def test_redirect_to_login(self):
         response = self.client.get(reverse('microblog:list'))
 
-        assert response.status_code == 200
+        assert response.status_code == 302
